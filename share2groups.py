5c319f25569164899fba45df0532a4fc48ca189f
zhiyuli <zyli2004@gmail.com>
2019-04-09 12:27:17 -0400

    udpate share2groups

--- share2groups.py
+++ share2groups.py
@@ -9,7 +9,7 @@ from hs_core.hydroshare import utils
 from hs_access_control.models import PrivilegeCodes
 
 # PrivilegeCodes.CHANGE or PrivilegeCodes.VIEW
-SHARE_WITH_GROUP_PRIVILEGE = PrivilegeCodes.CHANGE
+SHARE_WITH_GROUP_PRIVILEGE = PrivilegeCodes.VIEW
 
 # czo_XXXXX HS account names and namesake Group names
 user_group_mapping = {
@@ -25,11 +25,13 @@ user_group_mapping = {
     "czo_shale-hills": "CZO Shale-Hills",
 }
 
+success_counter = 0
+failure_counter = 0
 
 for u_name, g_name in user_group_mapping.items():
 
     try:
-        print("Sharing {}'s resource with Group {} with {} privilege".format(u_name,
+        print("Sharing {}'s resource with Group {} with privilege code {}".format(u_name,
                                                                              g_name,
                                                                              SHARE_WITH_GROUP_PRIVILEGE))
         # user obj
@@ -41,17 +43,25 @@ for u_name, g_name in user_group_mapping.items():
         # res owned by u
         owned_resources = u.uaccess.get_resources_with_explicit_access(PrivilegeCodes.OWNER)
 
+        czo_counter = 0
         for r in owned_resources:
             try:
+                czo_counter += 1
                 u.uaccess.share_resource_with_group(r, g, SHARE_WITH_GROUP_PRIVILEGE)
+                success_counter += 1
             except Exception as ex:
+                failure_counter += 1
                 print(ex.message)
+        print("Processed {} res of CZO {}".format(czo_counter, u_name))
 
     except Exception as ex:
+        failure_counter += 1
         print(ex.message)
 
 
-print("Done")
+print("Done! Total: {}; Success: {}; Failed: {}".format(success_counter+failure_counter,
+                                                        success_counter,
+                                                        failure_counter))
 
 
 
