5c72bf7acbfb9551224bc20f5c38f3801d69a6a7
Mark OBrien <mark_noreply@noreply.com>
2019-10-16 10:18:48 -0400

    Fix formatting

--- util.py
+++ util.py
@@ -115,24 +115,22 @@ def gen_readme(rowdata, related_resources):
             info += conditional_write('Publications using this data', _pub_using)
         info += normal_write("CZO ID", rowdata.get('czo_id'))
         info += conditional_write('Related datasets', rowdata.get('RELATED_DATASETS'))
-        ext_links = str(rowdata.get('EXTERNAL_LINKS-url$link_text'))
-        award_grants = str(rowdata.get('AWARD_GRANT_NUMBERS-grant_number$funding_agency$url_for_grant'))
 
+        ext_links = str(rowdata.get('EXTERNAL_LINKS-url$link_text'))
         if ext_links.lower() != 'nan' and ext_links.lower() != 'none':
             ext_links = ext_links.split('|')
             ext_links = ["<a href='{}' target='_blank'>{}</a> | ".format(x.split('$')[0], x.split('$')[1]) for x in ext_links]
             printable_links = " ".join(ext_links)
             info += conditional_write('External Links', printable_links)
 
+        award_grants = str(rowdata.get('AWARD_GRANT_NUMBERS-grant_number$funding_agency$url_for_grant'))
         if award_grants.lower() != 'nan' and award_grants.lower() != 'none':
             award_grants = award_grants.split('|')
             award_grants = ["<a href='{}' target='_blank'>{} - {}</a>".format(x.split('$')[2], x.split('$')[0], x.split('$')[1]) for x in award_grants]
             printable_award_grants = " \n\n".join(award_grants)
             info += conditional_write('Award Grant Numbers', printable_award_grants)
-            if ext_links.lower() != 'nan' and ext_links.lower() != 'none':
-                _ = 1
-            else:
-                info += "  \n<br /><br />\n  "
+            
+        info += "  \n<br /><br />\n  "
 
         _comments = str(rowdata.get('comments'))
         if _comments and _comments.lower() != 'nan' and _comments.lower() != 'none':
