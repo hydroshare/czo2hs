2b00fc16b0ad33ca8341ab059d9435358ca39e9a
Mark OBrien <mark_noreply@noreply.com>
2019-12-12 11:09:27 -0500

    Stop using hydro-service in manage.py

--- share2groups.sh
+++ share2groups.sh
@@ -2,5 +2,5 @@ echo "Copying scripts to hydroshare container"
 docker cp ./share2groups.py hydroshare:/tmp/share2groups.py
 docker cp ./settings.py hydroshare:/tmp/settings.py
 echo "Running script"
-docker exec -it -u hydro-service hydroshare bash -c "cd /tmp; python /hydroshare/manage.py shell < share2groups.py"
+docker exec -it hydroshare bash -c "cd /tmp; python /hydroshare/manage.py shell < share2groups.py"
 echo "Done"
